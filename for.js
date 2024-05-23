77ad03e3e763af62c75aadabac6f1b32734b8cde
Jenifer29b <Jenifer29b@gmail.com>
2024-05-23 15:53:25 +0530

    day5 task completed

---
+++ for.js
@@ -0,0 +1,14 @@
+var obj = {
+    name : "jenifer",
+    place : "chennai",
+    hobby : "listening music",
+    degree : "B.sc",
+    Aim : "to become a software engineer"
+ }
+
+ let arr = [obj];
+ console.log(arr);
+ 
+ for(let i=0; i<arr.length; i++){
+    console.log(arr[i]);
+ }
